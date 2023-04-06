4f6142188f88770dfb12a36f9962993841f02071
PortlyDragon <116568795+PortlyDragon@users.noreply.github.com>
2023-04-06 17:20:59 -0400

    Bullets

---
+++ KingfishersProjectAlpha/Assets/Scripts/Damage.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Damage 
+{
+    // Start is called before the first frame update
+   void TakeDamage(int damage);
+}
