9f1df36db66e013baea3c18103ef893f570d1f34
PortlyDragon <116568795+PortlyDragon@users.noreply.github.com>
2023-04-09 11:24:06 -0400

    Second gun

--- KingfishersProjectAlpha/Assets/Scripts/Gun.cs
+++ KingfishersProjectAlpha/Assets/Scripts/Gun.cs
@@ -24,7 +24,7 @@ public class Gun : MonoBehaviour
     {
         if (!isShooting && Input.GetButton("Shoot"))
         {
-            
+        
             StartCoroutine(shoot());
         }
     }
