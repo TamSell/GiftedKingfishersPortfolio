11361c1b3360bafbb17a616698a35b916655f1f8
PortlyDragon <116568795+PortlyDragon@users.noreply.github.com>
2023-04-11 18:38:08 -0400

    stamina

--- KingfishersProjectAlpha/Assets/Scripts/PlayerController.cs
+++ KingfishersProjectAlpha/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour , Damage
+public class PlayerController : MonoBehaviour, Damage
 {
 
     [Header("----- Components-----")]
     [SerializeField] CharacterController controller;
 
     [Header("----- Player Stats -----")]
-    [Range(3,8)] [SerializeField] float PlayerSpeed;
+    [Range(3, 8)][SerializeField] float PlayerSpeed;
     [Range(3, 30)][SerializeField] float jumpHeight;
     [Range(3, 25)][SerializeField] float gravityValue;
     [Range(1, 4)][SerializeField] int jumpMax;
     [Range(1, 20)][SerializeField] int HP;
-    [Range(1,200)][SerializeField] float RunFOV;
+    [Range(1, 200)][SerializeField] float RunFOV;
     [Range(0, 10)][SerializeField] float RunSpeed;
+    [Range(0,10)] [SerializeField] float Stamina;
 
 
     float FOVorg;
@@ -25,6 +26,7 @@ public class PlayerController : MonoBehaviour , Damage
     private bool groundedPlayer;
     Vector3 move;
     int HPorig;
+   
 
     // Start is called before the first frame update
     void Start()
@@ -53,13 +55,28 @@ public class PlayerController : MonoBehaviour , Damage
 
         if(Input.GetButton("Run"))
         {
-            Camera.main.fieldOfView = RunFOV;
-            controller.Move(move * Time.deltaTime * (PlayerSpeed + RunSpeed));
+            if(Stamina > 0)
+            {
+                Camera.main.fieldOfView = RunFOV;
+                controller.Move(move * Time.deltaTime * (PlayerSpeed + RunSpeed));
+                Stamina -= 3 * Time.deltaTime;
+               
+            }
+          
        }
         else
         {
-            Camera.main.fieldOfView = FOVorg;
-            controller.Move(move * Time.deltaTime * PlayerSpeed);
+            
+               
+                Camera.main.fieldOfView = FOVorg;
+                controller.Move(move * Time.deltaTime * PlayerSpeed);
+                if (Stamina <= 10)
+                {
+                    Stamina += 2 * Time.deltaTime;
+                }
+            
+           
+            
         }
       
 
