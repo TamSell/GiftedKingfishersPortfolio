ebf2214fd9d0f7b92d068d3291cf77d36d94db09
PortlyDragon <116568795+PortlyDragon@users.noreply.github.com>
2023-04-12 10:35:23 -0400

    Gun Realoading bug trying to fix

--- KingfishersProjectAlpha/Assets/Scripts/Gun.cs
+++ KingfishersProjectAlpha/Assets/Scripts/Gun.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
@@ -8,49 +9,73 @@ public class Gun : MonoBehaviour
    
     [Range(0.1f,2)][SerializeField] float ShootRate;
     [Range(0,30)][SerializeField] int MagazineInGun;
-    [Range(0,60)][SerializeField] int ammoCount;
-    
-    
-    
+    [Range(0,60)][SerializeField] int TotalAmmo;
+    [SerializeField]bool reaload;
+
+    float time = 1f;
+    float timer;
     int GunTotalAmmo;
-    [SerializeField]bool isShooting;
+    bool isShooting;
 
     int ammoToInsert;
     public GameObject bullet ;
     public Transform gun;
+  
+
 
     
 
     // Start is called before the first frame update
     void Start()
     {
+        timer = Time.time;
         GunTotalAmmo = MagazineInGun;
     }
 
     // Update is called once per frame
     void Update()
     {
-      Reloading();
-        if (MagazineInGun == 0)
+        if(MagazineInGun>GunTotalAmmo)
         {
-            return;
+            MagazineInGun = GunTotalAmmo;
         }
+
+        Reloading();
+  
+        if (reaload == true)
+        {
+            Invoke("shooting", 2);
+          
+        }
+        else
+        {
+            shooting();
+          
+        }
+     
+
+    }
+
+    public void shooting()
+    {
         if (!isShooting && Input.GetButton("Shoot"))
         {
-        
+
             StartCoroutine(shoot());
-       
+         
         }
     }
+   
+
 
     IEnumerator shoot()
     {
-    
+     
         isShooting = true;
 
      
         Instantiate(bullet, gun.position,gun.rotation);
-        
+     
 
         yield return new WaitForSeconds(ShootRate);
         isShooting = false;
@@ -66,23 +91,50 @@ public class Gun : MonoBehaviour
     {
         if (Input.GetButtonDown("Reloading"))
         {
-            if (ammoCount >= GunTotalAmmo)
-            {
-                ammoToInsert = GunTotalAmmo - MagazineInGun;
-                ammoCount -= ammoToInsert;
-                MagazineInGun += ammoToInsert;
-            }
-            else
-            {
-                ammoToInsert = ammoCount;
-                ammoCount -= ammoCount;
-                MagazineInGun += ammoToInsert;
-            }
+            reaload = true;
+            RealoadingLogic();
+            IsReloading();
+            reaload = false;
         }
-
+        else if (MagazineInGun==0 && TotalAmmo>0)
+        {
+            reaload = true;
+            RealoadingLogic();
+            IsReloading();
+            reaload=false;
+         
+        }
+       
+    }
+    public void RealoadingLogic()
+    {
+        if (TotalAmmo > GunTotalAmmo)
+        {
+            ammoToInsert = GunTotalAmmo - MagazineInGun;
+            TotalAmmo -= ammoToInsert;
+            MagazineInGun += ammoToInsert;
+        }
+        else if(MagazineInGun > TotalAmmo)
+        {
+            ammoToInsert = MagazineInGun - TotalAmmo;
+            TotalAmmo -= TotalAmmo;
+            MagazineInGun += ammoToInsert;
+        }
+        else if(MagazineInGun == 0)
+        {
+            ammoToInsert = TotalAmmo;
+            TotalAmmo -= TotalAmmo;
+            MagazineInGun += ammoToInsert;
+        }
     }
 
+    IEnumerator IsReloading()
+    {
+        reaload = true;
+        yield return new WaitForSeconds(2);
+        reaload = false;
 
+    }
 
 
 
