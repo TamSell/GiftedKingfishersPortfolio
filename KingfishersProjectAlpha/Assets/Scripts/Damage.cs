d15d069def46620287190435875a262962946e50
Aidan Nelson <AMNelson1@student.fullsail.edu>
2023-04-23 17:47:52 -0400

    Random Fix
    Duplicate Scripts appeared and were removed to fix the issue

--- KingfishersProjectAlpha/Assets/Scripts/Damage.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface Damage 
-{
-    // Start is called before the first frame update
-   void TakeDamage(int damage);
-}
