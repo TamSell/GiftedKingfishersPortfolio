fb5360d1556b9a9bc437522da0162fedf4ec4aeb
PortlyDragon <116568795+PortlyDragon@users.noreply.github.com>
2023-05-04 13:53:36 -0400

    working on shotgun

--- KingfishersProjectAlpha/Assets/Scripts/Guns and ammo/Gun.cs
+++ KingfishersProjectAlpha/Assets/Scripts/Guns and ammo/Gun.cs
@@ -23,6 +23,7 @@ public class Gun : MonoBehaviour
     [Header("---Shotgun-----")]
     [SerializeField] public bool shotgun;
     [SerializeField] public int bulletPerShot;
+    [SerializeField] public float inaccuracyDistance;
 
     [Header("----- Ammo -----")]
     [Range(0, 30)][SerializeField] public int magSize;
@@ -126,7 +127,7 @@ public class Gun : MonoBehaviour
             RaycastHit hit;
             if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, RayGunDist))
             {
-
+                
 
                 Damage damage = hit.collider.GetComponent<Damage>();
                 if (damage != null)
@@ -147,7 +148,7 @@ public class Gun : MonoBehaviour
                 if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out hit, RayGunDist))
                 {
 
-
+                    
                     Damage damage = hit.collider.GetComponent<Damage>();
                     if (damage != null)
                     {
@@ -231,6 +232,17 @@ public class Gun : MonoBehaviour
     {
         totalAmmo+= ammount;
         gameManager.Instance.loadText(totalAmmo, currentMag);
+    }
+
+    Vector3 GetShootingDiraction()
+    {
+        Vector3 targetPos = cam.position + cam.forward * RayGunDist;
+        targetPos = new Vector3(
+        targetPos.x = UnityEngine.Random.Range(-inaccuracyDistance, inaccuracyDistance),
+        targetPos.y = UnityEngine.Random.Range(-inaccuracyDistance, inaccuracyDistance),
+        targetPos.z = UnityEngine.Random.Range(-inaccuracyDistance, inaccuracyDistance));
+        Vector3 direction = targetPos - cam.position;
+        return direction.normalized;
     }
 
 }
