33427d2cdaf691490ecacc839ee35022206bcbf0
Aidan Nelson <AMNelson1@student.fullsail.edu>
2023-05-05 17:36:02 -0400

    Created ShotGun and Finished Modification UI
    Modification UI is fully present and a Gun prefab was created

--- KingfishersProjectAlpha/Assets/Scripts/UI and gameManager/Craft Slots.cs
+++ KingfishersProjectAlpha/Assets/Scripts/UI and gameManager/Craft Slots.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Crafting : MonoBehaviour
+public class CraftSlots : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
