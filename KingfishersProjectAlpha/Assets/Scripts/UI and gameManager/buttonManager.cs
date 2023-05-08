453b691e866c8b7e8e6b407e1cf17226b3282793
VictorV808 <VMVandoSantiago@student.fullsail.edu>
2023-05-08 13:48:30 -0400

    Audio Feedback Buttons

--- KingfishersProjectAlpha/Assets/Scripts/UI and gameManager/buttonManager.cs
+++ KingfishersProjectAlpha/Assets/Scripts/UI and gameManager/buttonManager.cs
@@ -3,30 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class buttonManager : MonoBehaviour
 {
     int place;
+    [SerializeField] AudioSource aud;
+    [SerializeField] AudioClip button;
+    [Range(0, 1)][SerializeField] float audButtonVol;
 
     public void resume()
     {
+        aud.PlayOneShot(button, audButtonVol);
         gameManager.Instance.unpause();
         gameManager.Instance.inMenu = !gameManager.Instance.inMenu;
     }
 
     public void restart()
     {
+        aud.PlayOneShot(button, audButtonVol);
         gameManager.Instance.unpause();
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void Save()
     {
+        aud.PlayOneShot(button, audButtonVol);
         saveLoadManager.Save();
     }
 
     public void quit()
     {
+        aud.PlayOneShot(button, audButtonVol);
         Application.Quit();
     }
     public void Respawnplayer()
