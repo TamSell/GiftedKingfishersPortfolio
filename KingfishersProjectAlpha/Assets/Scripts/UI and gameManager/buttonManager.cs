8ef2278717edcdea810abece48b9dd7ffaa18761
PortlyDragon <116568795+PortlyDragon@users.noreply.github.com>
2023-05-17 10:31:23 -0400

    lose menu

--- KingfishersProjectAlpha/Assets/Scripts/UI and gameManager/buttonManager.cs
+++ KingfishersProjectAlpha/Assets/Scripts/UI and gameManager/buttonManager.cs
@@ -49,7 +49,7 @@ public class buttonManager : MonoBehaviour
     public void Respawnplayer()
     {
         gameManager.Instance.unpause();
-        //gameManager.Instance.playerController.respawnPlayer();
+        gameManager.Instance.playerController.respawnPlayer();
     }
 
     public void itemClick()
